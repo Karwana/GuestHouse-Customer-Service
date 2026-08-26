68797eac644b52fe05c40e7f074f859d4c9f79a5
Karwan Ali <karwana0000@gmail.com>
2026-08-26 19:36:36 +0200

    Added integrationtesting for creating valid customer

---
+++ src/test/java/org/nackademin/guesthousecustomerservice/CustomerIntegrationTest.java
@@ -0,0 +1,36 @@
+package org.nackademin.guesthousecustomerservice;
+
+import org.junit.jupiter.api.Test;
+import org.nackademin.guesthousecustomerservice.entity.Customer;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.webmvc.test.autoconfigure.AutoConfigureMockMvc;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+public class CustomerIntegrationTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    void return201WhenCreatingCustomer() throws Exception {
+
+        String validCustomerJson = """
+                {
+                                    "name": "Karwan Ali",
+                                    "email": "karwan@integrationtest.com",
+                                    "phoneNumber": "0701234567"
+                                }
+             """;
+        mockMvc.perform(post("/api/customers")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(validCustomerJson))
+                .andExpect(status().isCreated());
+    }
+}
