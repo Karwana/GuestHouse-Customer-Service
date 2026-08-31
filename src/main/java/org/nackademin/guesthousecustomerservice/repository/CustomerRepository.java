b08543c9373e5946768203ef77e64802c9492955
Karwan Ali <karwana0000@gmail.com>
2026-08-31 22:25:57 +0200

    line cleanup

--- src/main/java/org/nackademin/guesthousecustomerservice/repository/CustomerRepository.java
+++ src/main/java/org/nackademin/guesthousecustomerservice/repository/CustomerRepository.java
@@ -3,6 +3,5 @@ package org.nackademin.guesthousecustomerservice.repository;
 import org.nackademin.guesthousecustomerservice.entity.Customer;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface CustomerRepository
-        extends JpaRepository<Customer, Long> {
+public interface CustomerRepository extends JpaRepository<Customer, Long> {
 }
